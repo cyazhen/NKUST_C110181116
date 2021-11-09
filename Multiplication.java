93822cc270e4705045cc8ed1c1f5a49e26294e7c
Jerry <C110181116@nkust.edu.tw>
2021-11-09 23:34:18 +0800

    Jerry

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        for(int i = 1;i <= 9;i++){
+            for(int j = 1; j <= 9;j++) {
+                int sum = i * j;
+                System.out.println(i + " X " + j + "=" + i*j );
+                if (j==9){
+                    System.out.println("\t");
+                }
+            }
+        }
+    }
+}
