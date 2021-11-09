d7ea0449a5d87f301b9d88cb9d43195403799c24
Jerry <C110181116@nkust.edu.tw>
2021-11-09 22:38:31 +0800

    6

--- BMI.java
+++ BMI.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class BMI {
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        float hight;
+        int weight;
+        float sum;
+        Scanner H =new Scanner(System.in);
+        Scanner W =new Scanner(System.in);
+
+        System.out.println("請輸入身高: ");
+        hight = H.nextInt();
+        hight = hight / 100;
+        System.out.println("請輸入體重: ");
+        weight = W.nextInt();
+        sum = weight / (hight * hight);
+        System.out.println("你的BMI是 " + sum);
+    }
 }
---
+++ Six.java
@@ -0,0 +1,10 @@
+public class Six {
+    public static void main(String[] args) {
+        int i;
+        for(i = 1;i <= 300;i++) {
+            if(i % 6 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
