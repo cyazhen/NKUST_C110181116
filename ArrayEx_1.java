8a61f37442150fb78746b4f4d2d1d291d09fd28d
Jerry <C110181116@nkust.edu.tw>
2021-11-16 02:28:14 +0800

    Jerry

--- ArrayEx_1.java
+++ ArrayEx_1.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class ArrayEx_1 {
+import java.util.*;
+
+public class ArrayEx_1 {
+    public static void main(String[] args) {
+        List<Integer> arrlist = new ArrayList<>();
+        List<Integer> Rndlist = new ArrayList<>();
+        int []arrnum = {2,7,11,15};
+        arrlist.add(arrnum[0]);
+        arrlist.add(arrnum[1]);
+        arrlist.add(arrnum[2]);
+        arrlist.add(arrnum[3]);
+        for (int i = 3; i < 4; i++){
+            Rndlist.add ((int)(Math.random() * i));
+            Rndlist.add ((int)(Math.random() * i));
+            if (Rndlist.get(0) == Rndlist.get(1)){
+                Rndlist.clear();
+                i = 3;
+                continue;
+            }
+        }
+        Collections.sort(Rndlist);
+        int Target = arrnum[Rndlist.get(0)] + arrnum[Rndlist.get(1)];
+        System.out.println(arrlist);
+        System.out.println("Target= " + Target);
+        System.out.println(Rndlist);
+    }
 }
--- Test.java
+++ Test.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Test {
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        int []arrnum = {2,7,11,15};
+        list.add(arrnum[0]);
+        list.add(arrnum[1]);
+        list.add(arrnum[2]);
+        list.add(arrnum[3]);
+        Collections.sort(list);
+        System.out.println(list.get(0));
+        for (int n : list)
+            System.out.println(n);
+    }
 }
