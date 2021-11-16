37c0612ad1143efa49a65d1861252fb097b8f06b
Jerry <C110181116@nkust.edu.tw>
2021-11-16 17:35:19 +0800

    prime

--- prime.java
+++ prime.java
@@ -1,13 +1,19 @@
+import java.util.*;
+
 public class prime {
     public static void main(String[] args) {
+        List<Integer> length = new ArrayList<>();
         int i , j;
-        for (i = 2; i <= 100; i++) {
-            for (j = 2; j < i; j++) {
-                if (i % j == 0)
+        for (i = 2; i <= 100; i++) {    //i,被除數
+            for (j = 2; j < i; j++) {   //j,除數
+                if (i % j == 0)    //取最小因數
                     break;
             }
-            if (j >= i)
+            if (j == i){    //判斷最小因數是否為自己
+                length.add(i);
                 System.out.println(j);
+            }
         }
+        System.out.printf("總共有: %d 個數為質數" , length.size());
     }
 }
