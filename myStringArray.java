6ff66c8c726ac5c9bc6ef52274a052652ecd002a
Jerry <C110181116@nkust.edu.tw>
2021-11-21 00:55:28 +0800

    StringArray

--- myStringArray.java
+++ myStringArray.java
@@ -4,21 +4,21 @@ public class myStringArray {
     public static void main(String[] args) {
         int outputResult = 0 , norepeatCount = 0;
         String inputStr;
-        Map<Character , Integer> strPick =new HashMap<>();
+        Map<Character , Integer> strPickMap =new HashMap<>();
         Scanner strInput = new Scanner(System.in);
         System.out.println("請輸入字串：");
         inputStr = strInput.nextLine();
 
         for(int i = 0 ; i < inputStr.length() ; i++){
             char charGet = inputStr.charAt(i);
-            if (strPick.containsKey(charGet) == false) {
+            if (strPickMap.containsKey(charGet) == false) {
                 norepeatCount++;
             }
             else{
                 outputResult = Math.max(norepeatCount , outputResult);
                 norepeatCount = 1;
             }
-            strPick.put(charGet,i);
+            strPickMap.put(charGet,i);
         }
         System.out.println("Output：" + outputResult);
     }
