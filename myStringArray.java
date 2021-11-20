821366c0be1cc91766d1d15bf45b2b6d77670523
Jerry <C110181116@nkust.edu.tw>
2021-11-21 01:06:03 +0800

    StringArray

--- myStringArray.java
+++ myStringArray.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class myStringArray {
     public static void main(String[] args) {
-        int outputResult = 0 , norepeatCount = 0;
+        int outputResult = 0 , noRepeatCount = 0;
+        //noRepeatCount為計算不重複的字元長度
+        //outputResult為儲存最長不重複字元
         String inputStr;
         Map<Character , Integer> strPickMap =new HashMap<>();
         Scanner strInput = new Scanner(System.in);
@@ -11,12 +13,12 @@ public class myStringArray {
 
         for(int i = 0 ; i < inputStr.length() ; i++){
             char charGet = inputStr.charAt(i);
-            if (strPickMap.containsKey(charGet) == false) {
-                norepeatCount++;
+            if (strPickMap.containsKey(charGet) == false) {     //判斷Map是否有字元的存在
+                noRepeatCount++;                                //不存在的話，noRepeatCount累加
             }
             else{
-                outputResult = Math.max(norepeatCount , outputResult);
-                norepeatCount = 1;
+                outputResult = Math.max(noRepeatCount , outputResult);  //如出現重複字元，則將結果與累加的值進行比較，取最大值
+                noRepeatCount = 1;  //不重複字元長度，將由當前的字元開始重新累加
             }
             strPickMap.put(charGet,i);
         }
