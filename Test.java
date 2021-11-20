c64a726155ae97e1ff4b9defc973383548275a35
Jerry <C110181116@nkust.edu.tw>
2021-11-21 00:51:16 +0800

    StringArray

--- Test.java
+++ Test.java
@@ -2,6 +2,24 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
+        int outputResult = 0 , norepeatCount = 0;
+        String inputStr;
+        Map<Character , Integer> strPick =new HashMap<>();
+        Scanner strInput = new Scanner(System.in);
+        System.out.println("請輸入字串：");
+        inputStr = strInput.nextLine();
 
+        for(int i = 0 ; i < inputStr.length() ; i++){
+            char charGet = inputStr.charAt(i);
+            if (strPick.containsKey(charGet) == false) {
+                norepeatCount++;
+            }
+            else{
+                outputResult = Math.max(norepeatCount , outputResult);
+                norepeatCount = 1;
+            }
+            strPick.put(charGet,i);
+        }
+        System.out.println("Output：" + outputResult);
     }
 }
--- stringArray.java
+++ stringArray.java
@@ -1,25 +1,23 @@
 import java.util.*;
 
 public class stringArray {
-    static String strGet;
     public static void main(String[] args) {
-        inPut();
-        int strLength = strGet.length() , outPut = 0 ;
-        Map <Character,Integer> strPick = new HashMap();
-
-        for (int end = 0 , start = 0 ; end < strLength ; end++){
-            char charGet = strGet.charAt(end);          //抓出strGet的字元
-            if (strPick.containsKey(charGet)){          //判斷是否出現重複字元
-                start = Math.max(start,strPick.get(charGet)+1);
-            }
-            outPut = Math.max(end - start +1,outPut);   //比較長度，取最長
-            strPick.put(charGet,end);
-        }
-        System.out.println("Output: " + outPut);
-    }
-    static void inPut(){
+        String inputStr;
         Scanner strInput = new Scanner(System.in);
         System.out.println("請輸入字串：");
-        strGet = strInput.nextLine();
+        inputStr = strInput.nextLine();
+        int strLength = inputStr.length() , outputResult = 0 , start = 0 ;
+        Map <Character,Integer> strPick = new HashMap();
+
+        for (int end = 0 ; end < strLength ; end++){
+            char charGet = inputStr.charAt(end);          //抓出strGet的字元
+            if (strPick.containsKey(charGet)){          //判斷是否出現重複字元
+                start =strPick.get(charGet)+1;
+            }
+            outputResult = Math.max(end - start +1,outputResult);   //比較長度，取最長
+            strPick.put(charGet,end);
+        }
+        System.out.println("Output: " + outputResult);
+        System.out.println(strPick);
     }
 }
