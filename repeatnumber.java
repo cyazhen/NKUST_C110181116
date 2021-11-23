7e99e70ea997efb2039e19a65964d8adafb4dfe0
Jerry <C110181116@nkust.edu.tw>
2021-11-23 16:24:51 +0800

    11/23

--- repeatnumber.java
+++ repeatnumber.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class repeatnumber {
+import java.util.*;
+
+public class repeatnumber {
+    public static void main(String[] args) {
+        Map<Integer,Character> pickNumMap = new HashMap<>();
+        Scanner strInput = new Scanner(System.in);
+        System.out.println("請輸入字串：");
+        String inputNum = strInput.nextLine();
+
+        for (int i = 0 ; i < inputNum.length() ; i++){
+            char getNum = inputNum.charAt(i);
+            if (pickNumMap.containsValue(getNum) == true){
+                pickNumMap.putIfAbsent(i,);
+            }
+            pickNumMap.put(i,getNum);
+        }
+
+    }
 }
--- unHash.java
+++ unHash.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class unHash {
+import java.util.*;
+
+class UnHash{
+
+    public static void main (String[] args) {
+        System.out.println(revHash(6933552791181934L));
+        System.out.println(hash("justdoit")); //574318821802
+    }
+
+    public static String letters = "cdefghijlmnoqstuvxz";
+
+    public static String revHash(long hash) {
+        List<Character> numGet = new ArrayList();
+        while (true){
+            for (int i = 0; i < letters.length(); i++){
+                if ((hash - i) % 23 == 0){
+                    hash = (hash - letters.indexOf(i)) / 23;
+                    numGet.add(letters.charAt(i));
+                    break;
+                }
+            }
+            if (hash == 7){
+                break;
+            }
+        }
+        return (numGet.toString());
+    }
+
+    public static long hash(String s){
+        long h = 7;
+        for (int i = 0; i < s.length(); i++){
+            h = h * 23 + letters.indexOf(s.charAt(i));
+        }
+        return h;
+    }
 }
