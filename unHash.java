4b5a94a7add04cf43b1185059b2687b7c5d6141c
Jerry <C110181116@nkust.edu.tw>
2021-11-23 22:13:52 +0800

    11/23

--- unHash.java
+++ unHash.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 class UnHash{
-
     public static void main (String[] args) {
         System.out.println(revHash(6933552791181934L));
         System.out.println(hash("justdoit")); //574318821802
@@ -10,12 +9,15 @@ class UnHash{
     public static String letters = "cdefghijlmnoqstuvxz";
 
     public static String revHash(long hash) {
-        List<Character> numGet = new ArrayList();
+        List<Character> correctChr = new ArrayList();
+        List<Character> output = new ArrayList<>();
+        //chrGet取得正確字元
+        //output字元排序校正
         while (true){
             for (int i = 0; i < letters.length(); i++){
-                if ((hash - i) % 23 == 0){
-                    hash = (hash - letters.indexOf(i)) / 23;
-                    numGet.add(letters.charAt(i));
+                if ((hash - i) % 23 == 0){                      //以0開始取數直到letters的長度，進行運算，用來判斷字元正確位置
+                    hash = (hash - letters.indexOf(i)) / 23;    //替換數字用來判斷下一個字元的位置
+                    correctChr.add(letters.charAt(i));              //將正確字元放進correctChr
                     break;
                 }
             }
@@ -23,7 +25,10 @@ class UnHash{
                 break;
             }
         }
-        return (numGet.toString());
+        for (int i = correctChr.size() - 1 ; i >= 0 ; i--){     //將字元排序校正
+            output.add(correctChr.get(i));                      //例：peels 校正為 sleep
+        }
+        return (output.toString());
     }
 
     public static long hash(String s){
