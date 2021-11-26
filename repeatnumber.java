3c6a979c9136c4ec04c0bc67d8bbc579d85587e8
C110181116 <93572311+C110181116@users.noreply.github.com>
2021-11-26 14:18:45 +0800

    Update repeatnumber.java

--- repeatnumber.java
+++ repeatnumber.java
@@ -4,23 +4,23 @@ public class repeatnumber {
     public static void main(String[] args) {
         Map<Character,Integer> checkRepeatMap = new HashMap<>();
         List<String> outputResult = new ArrayList<>();
-        int noRepeatCount = 0;                                //repeatCount用來計算重複的數量
+        int noRepeatCount = 0;                                //noRepeatCount用來計算不重複數字的數量
         Scanner strInput = new Scanner(System.in);
         System.out.println("請輸入字串：");
         String inputNum = strInput.nextLine();
 
         for (int i = 0 ; i < inputNum.length() ; i++){
-            char getNum = inputNum.charAt(i);               //getNum拿取inputNum裡的數值
-            if (!checkRepeatMap.containsKey(getNum)){       //透過checkRepeatMap判斷數值是否重複
+            char getNum = inputNum.charAt(i);                //getNum拿取inputNum裡的數值
+            if (!checkRepeatMap.containsKey(getNum)){        //透過checkRepeatMap判斷數值是否重複
                 outputResult.add(String.valueOf(getNum));    //如不重複將當前數值加進outputResult
-                noRepeatCount = noRepeatCount + 1;              //並且repeatCount +1
+                noRepeatCount = noRepeatCount + 1;           //並且noRepeatCount +1
             }
             else{
-                outputResult.add("_");                      //重複的話將一個底線加至outputResult
+                outputResult.add("_");                       //重複的話將一個底線加至outputResult
             }
-            checkRepeatMap.put(getNum,i);                   //將getNum的值放進checkRepeatMap，用於if判斷是否重複
+            checkRepeatMap.put(getNum,i);                    //將getNum的值放進checkRepeatMap，用於if判斷是否重複
         }
-        Collections.sort(outputResult);                     //整理outputResult
+        Collections.sort(outputResult);                      //整理outputResult
         System.out.println("nums = " + outputResult);
         System.out.println("Output：" + noRepeatCount);
     }
