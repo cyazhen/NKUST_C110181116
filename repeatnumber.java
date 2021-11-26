7214d4cb6f48bf73b1fcb64d5953b4d0077d76c0
Jerry <C110181116@nkust.edu.tw>
2021-11-26 14:17:23 +0800

    11/26

--- repeatnumber.java
+++ repeatnumber.java
@@ -4,7 +4,7 @@ public class repeatnumber {
     public static void main(String[] args) {
         Map<Character,Integer> checkRepeatMap = new HashMap<>();
         List<String> outputResult = new ArrayList<>();
-        int repeatCount = 0;                                //repeatCount用來計算重複的數量
+        int noRepeatCount = 0;                                //repeatCount用來計算重複的數量
         Scanner strInput = new Scanner(System.in);
         System.out.println("請輸入字串：");
         String inputNum = strInput.nextLine();
@@ -12,16 +12,16 @@ public class repeatnumber {
         for (int i = 0 ; i < inputNum.length() ; i++){
             char getNum = inputNum.charAt(i);               //getNum拿取inputNum裡的數值
             if (!checkRepeatMap.containsKey(getNum)){       //透過checkRepeatMap判斷數值是否重複
-                outputResult.add(String.valueOf(getNum));   //如不重複將當前數值加進outputResult
+                outputResult.add(String.valueOf(getNum));    //如不重複將當前數值加進outputResult
+                noRepeatCount = noRepeatCount + 1;              //並且repeatCount +1
             }
             else{
                 outputResult.add("_");                      //重複的話將一個底線加至outputResult
-                repeatCount = repeatCount + 1;              //並且repeatCount +1
             }
             checkRepeatMap.put(getNum,i);                   //將getNum的值放進checkRepeatMap，用於if判斷是否重複
         }
         Collections.sort(outputResult);                     //整理outputResult
         System.out.println("nums = " + outputResult);
-        System.out.println("Output：" + repeatCount);
+        System.out.println("Output：" + noRepeatCount);
     }
 }
