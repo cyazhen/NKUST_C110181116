d5a871ea2e5664b7b80033acbb8a279d70d9f852
Jerry <C110181116@nkust.edu.tw>
2021-11-27 12:53:54 +0800

    11/27

--- prime.java
+++ prime.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 public class prime {
     public static void main(String[] args) {
-        List<Integer> primeArray = new ArrayList<>();
-        int i , j;
+        int i , j , primeCount = 0;     //primeCount計算質數的總數
         for (i = 2; i <= 100; i++) {    //i,被除數
             for (j = 2; j < i; j++) {   //j,除數
                 if (i % j == 0)         //取因數
                     break;
             }
             if (j == i){                //判斷因數是否為自己
-                primeArray.add(i);
+                primeCount = primeCount + 1;
                 System.out.println(j);
             }
         }
-        System.out.printf("總共有%d個數為質數" , primeArray.size());
+        System.out.printf("總共有%d個數為質數" , primeCount);
     }
 }
