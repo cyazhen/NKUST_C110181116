a84884552421ba43c522f701491df5becacd6606
Jerry <C110181116@nkust.edu.tw>
2021-11-30 16:12:57 +0800

    11/30

--- WebCrawler/src/main/java/WebCrawler.java
+++ WebCrawler/src/main/java/WebCrawler.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class WebCrawler {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class WebCrawler {
+    public static void main(String[] args) {
+        try {
+            Document doc = Jsoup.connect("https://rule.nkust.edu.tw/").get();
+            System.out.println(doc.title());
+            Elements newsHeadlines = doc.select("#Dyn_2_2 a");
+            for (Element headline : newsHeadlines) {
+                System.out.printf("%s\n%s\n",headline.attr("title"), headline.absUrl("href"));
+            }
+        }catch(Exception e){
+
+        }
+    }
 }
