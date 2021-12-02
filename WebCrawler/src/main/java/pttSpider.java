3d68e0f3391840f567e1fc596ece07b32e4f1d5f
Jerry <C110181116@nkust.edu.tw>
2021-12-02 20:50:15 +0800

    12/02

--- WebCrawler/src/main/java/pttSpider.java
+++ WebCrawler/src/main/java/pttSpider.java
@@ -11,13 +11,23 @@ public class pttSpider {
             Document doc = Jsoup.connect("https://www.ptt.cc/bbs/hotboards.html").get();
             Elements hotClasses = doc.select("#main-container .b-ent a");
             for (Element hotClass : hotClasses){
-                String pttTitle = hotClass.select("div:contains(◎)").text();                //最後輸出文章分類
-                String articleDoc = hotClass.absUrl("href").toString();
-                Document hotBoardDoc = Jsoup.connect(articleDoc);
-
-                Elements articleClasses = hotBoardDoc.select("#main-container .r-ent .title a");
-                for (Element articleClass : articleClasses){
-                    System.out.println(articleClass.absUrl("href"));
+                String pttTitle = hotClass.select("div:contains(◎)").text();       //最後輸出文章分類
+                String articleSortDoc = hotClass.absUrl("href").toString();
+                Document hotBoardDoc = Jsoup.connect(articleSortDoc).get();
+                Elements articleSortClasses = hotBoardDoc.select("#main-container .r-ent .title a");
+                for (Element articleClass : articleSortClasses){
+                    String articleTitle = articleClass.text();                             //最後輸出文章標題
+                    String articleDoc = articleClass.absUrl("href").toString();
+                    Document articleClasses = Jsoup.connect(articleDoc).get();
+                    Elements articleElements = articleClasses.select("#main-container #main-content");
+                    for(Element articleElement : articleElements){
+                        Element articleCommits = articleElement.select(" .push").first();
+                        Elements commitsName = articleCommits.select(".f3");
+                        System.out.println("Board: " + pttTitle);
+                        System.out.println("Article title: " + articleTitle);
+                        System.out.println("commit: " + commitsName.text());
+                        System.out.println("=======================");
+                    }
                 }
             }
         }catch (Exception e){
