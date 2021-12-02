0243b13cbb91664b16d3df08fa131ed28e986e6f
C110181116 <93572311+C110181116@users.noreply.github.com>
2021-12-02 19:44:30 +0800

    Update pttSpider.java

--- WebCrawler/src/main/java/pttSpider.java
+++ WebCrawler/src/main/java/pttSpider.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class pttSpider {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class pttSpider {
+    public static void main(String[] args) throws IOException {
+        try{
+            Document doc = Jsoup.connect("https://www.ptt.cc/bbs/hotboards.html").get();
+            Elements hotClasses = doc.select("#main-container .b-ent a");
+            for (Element hotClass : hotClasses){
+                String pttTitle = hotClass.select("div:contains(◎)").text();                //最後輸出文章分類
+                String articleDoc = hotClass.absUrl("href").toString();
+                Document hotBoardDoc = Jsoup.connect(articleDoc);
+
+                Elements articleClasses = hotBoardDoc.select("#main-container .r-ent .title a");
+                for (Element articleClass : articleClasses){
+                    System.out.println(articleClass.absUrl("href"));
+                }
+            }
+        }catch (Exception e){
+
+        }
+    }
 }
