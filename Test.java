bf8bdacdf3e778fcda54d12ff4866e9f33a9e548
Jerry <C110181116@nkust.edu.tw>
2021-12-03 23:48:47 +0800

    12/03

--- Test.java
+++ Test.java
@@ -1,25 +1,34 @@
 import java.util.*;
 
-public class Test {
-    public static void main(String[] args) {
-        int outputResult = 0 , norepeatCount = 0;
-        String inputStr;
-        Map<Character , Integer> strPick =new HashMap<>();
-        Scanner strInput = new Scanner(System.in);
-        System.out.println("請輸入字串：");
-        inputStr = strInput.nextLine();
+public class Test{
 
-        for(int i = 0 ; i < inputStr.length() ; i++){
-            char charGet = inputStr.charAt(i);
-            if (strPick.containsKey(charGet) == false) {
-                norepeatCount++;
-            }
-            else{
-                outputResult = Math.max(norepeatCount , outputResult);
-                norepeatCount = 1;
-            }
-            strPick.put(charGet,i);
+    public static void main (String[] args) {
+        System.out.println(revHash(6933552791181934L));
+        System.out.println(hash("justdoit")); //574318821802
+    }
+
+    public static String letters = "cdefghijlmnoqstuvxz";
+
+    public static String revHash(long hash) {
+        String result="";
+        String newResult="";
+
+        while(hash != 7){
+            result = result + letters.charAt((int) (hash % 23));
+            hash = hash/ 23;
         }
-        System.out.println("Output：" + outputResult);
+
+        for (int i = result.length(); i>=1; i--){
+            newResult += result.charAt(i-1);
+        }
+        return newResult;
+    }
+
+    public static long hash(String s){
+        long h = 7;
+        for (int i = 0; i < s.length(); i++){
+            h = h * 23 + letters.indexOf(s.charAt(i));
+        }
+        return h;
     }
 }
