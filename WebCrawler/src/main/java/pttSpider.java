dadee8f8a5ef878b4e36dba2772f523cbba91025
Jerry <C110181116@nkust.edu.tw>
2021-12-08 02:15:22 +0800

    Finish and Comment Out 12/08

--- WebCrawler/src/main/java/pttSpider.java
+++ WebCrawler/src/main/java/pttSpider.java
@@ -8,31 +8,42 @@ import java.io.IOException;
 public class pttSpider {
     public static void main(String[] args) throws IOException {
         try{
+            //抓取ptt熱門看板資料
             Document doc = Jsoup.connect("https://www.ptt.cc/bbs/hotboards.html").get();
+            //抓取ptt每個熱門看板網址
             Elements hotClasses = doc.select("#main-container .b-ent a");
             for (Element hotClass : hotClasses){
-                String pttTitle = hotClass.select("div:contains(◎)").text();       //最後輸出文章分類
+                //最後輸出文章分類
+                String pttTitle = hotClass.select("div:contains(◎)").text();
+                //讀取每個熱門看板網址
                 String articleSortDoc = hotClass.absUrl("href").toString();
                 Document hotBoardDoc = Jsoup.connect(articleSortDoc).get();
+                //抓取每個文章網址
                 Elements articleSortClasses = hotBoardDoc.select("#main-container .r-ent .title a");
                 for (Element articleClass : articleSortClasses){
-                    System.out.println(articleClass.absUrl("href"));
-                    String articleTitle = articleClass.text();                             //最後輸出文章標題
+                    //最後輸出文章標題
+                    String articleTitle = articleClass.text();
+                    //讀取每個文章網址
                     String articleDoc = articleClass.absUrl("href").toString();
                     Document articleClasses = Jsoup.connect(articleDoc).get();
-                    Elements articleElements = articleClasses.select("#main-container #main-content");
+                    //抓取每個文章留言
+                    Elements articleElements = articleClasses.select("#main-container #main-content .push");
                     for(Element articleElement : articleElements){
-                        Element articleCommits = articleElement.select(" .push").first();
-                        Elements commitsName = articleCommits.select(".f3");
+                        //抓出每個文章第一個留言資料
+                        Element firstPush = articleElements.get(0);
+                        String commitName = firstPush.select("span").get(1).text();
+                        String firstCommit = firstPush.select("span").get(2).text();
+                        //結果輸出
                         System.out.println("Board: " + pttTitle);
                         System.out.println("Article title: " + articleTitle);
-                        System.out.println("commit: " + commitsName.text());
-                        System.out.println("=======================");
+                        System.out.println("commit: " + commitName + firstCommit);
+                        System.out.println("==================================");
+                        break;
                     }
                 }
             }
         }catch (Exception e){
-
+            System.out.println("error:" + e);
         }
     }
 }
