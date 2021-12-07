bc01cc9d77bec1b94bfa8d7f84989441767719e0
Jerry <C110181116@nkust.edu.tw>
2021-12-08 02:57:15 +0800

    Finish and Commit Out 12/08

--- WebCrawler/src/main/java/pttSpider.java
+++ WebCrawler/src/main/java/pttSpider.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 
 public class pttSpider {
     public static void main(String[] args) throws IOException {
-        try{
+        try {
             //抓取ptt熱門看板資料
             Document doc = Jsoup.connect("https://www.ptt.cc/bbs/hotboards.html").get();
             //抓取ptt每個熱門看板網址
             Elements hotClasses = doc.select("#main-container .b-ent a");
-            for (Element hotClass : hotClasses){
+            for (Element hotClass : hotClasses) {
                 //最後輸出文章分類
                 String pttTitle = hotClass.select("div:contains(◎)").text();
                 //讀取每個熱門看板網址
@@ -20,29 +20,36 @@ public class pttSpider {
                 Document hotBoardDoc = Jsoup.connect(articleSortDoc).get();
                 //抓取每個文章網址
                 Elements articleSortClasses = hotBoardDoc.select("#main-container .r-ent .title a");
-                for (Element articleClass : articleSortClasses){
+                for (Element articleClass : articleSortClasses) {
                     //最後輸出文章標題
                     String articleTitle = articleClass.text();
                     //讀取每個文章網址
                     String articleDoc = articleClass.absUrl("href").toString();
                     Document articleClasses = Jsoup.connect(articleDoc).get();
                     //抓取每個文章留言
-                    Elements articleElements = articleClasses.select("#main-container #main-content .push");
-                    for(Element articleElement : articleElements){
-                        //抓出每個文章第一個留言資料
-                        Element firstPush = articleElements.get(0);
+                    Elements articleElements = articleClasses.select("#main-container #main-content");
+                    //確認是否有留言
+                    if (!articleElements.select(".push").isEmpty()) {
+                        //有的話則，抓出每個文章第一個留言資料
+                        Element firstPush = articleElements.select(".push").get(0);
                         String commitName = firstPush.select("span").get(1).text();
                         String firstCommit = firstPush.select("span").get(2).text();
-                        //結果輸出
+                        //輸出結果
                         System.out.println("Board: " + pttTitle);
                         System.out.println("Article title: " + articleTitle);
                         System.out.println("commit: " + commitName + firstCommit);
                         System.out.println("==================================");
-                        break;
+                    }
+                    else{
+                        //沒有的話則，commit為null，並結果輸出
+                        System.out.println("Board: " + pttTitle);
+                        System.out.println("Article title: " + articleTitle);
+                        System.out.println("commit: null");
+                        System.out.println("==================================");
                     }
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("error:" + e);
         }
     }
