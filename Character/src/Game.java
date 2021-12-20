504aee12c6841a7db92e7440e3e4c0af01ea82e4
Jerry <C110181116@nkust.edu.tw>
2021-12-20 18:23:36 +0800

    榮commited

--- Character/src/Game.java
+++ Character/src/Game.java
@@ -1,9 +1,22 @@
-import javax.swing.*;
 import java.util.*;
 
 public class Game {
     public static void main(String[] args) {
         berserker Berserker =new berserker("狂戰士",30,5,15,"斬擊","橫掃");
-        Berserker.getCurrentStatus();
+        System.out.println("輸入職業代碼：1.狂戰士 2.弓箭手 3.魔法師");
+        Scanner chooseCareer = new Scanner(System.in);
+        if(chooseCareer.hasNextInt()){
+            switch (chooseCareer.nextInt()){
+                case 1:
+                    Berserker.getCurrentStatus();
+                case 2:
+                    //Archer.getCurrentStatus();
+                case 3:
+                    //Magician.getCurrentStatus();
+            }
+        }
+        else{
+            System.out.println("代碼輸入錯誤");
+        }
     }
 }
