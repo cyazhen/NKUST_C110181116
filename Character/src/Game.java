b63751412cd6f617180b7d3368bc077cc172466f
cyazhen <thaole89720@gmail.com>
2021-12-20 20:10:44 +0800

    陳雅蓁commited

--- Character/src/Game.java
+++ Character/src/Game.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Game {
     public static void main(String[] args) {
         berserker Berserker =new berserker("狂戰士",30,5,15,"斬擊","橫掃");
+        magician Magician =new magician("魔法師",10,40,30,"治癒","小火球");
         System.out.println("輸入職業代碼：1.狂戰士 2.弓箭手 3.魔法師");
         Scanner chooseCareer = new Scanner(System.in);
         if(chooseCareer.hasNextInt()){
@@ -12,7 +13,7 @@ public class Game {
                 case 2:
                     //Archer.getCurrentStatus();
                 case 3:
-                    //Magician.getCurrentStatus();
+                    Magician.getCurrentStatus();
             }
         }
         else{
